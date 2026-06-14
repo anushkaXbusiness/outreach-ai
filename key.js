a5c84657ee77a80b9a6880cd4cc316ecb90b08d7
Aanchal Wadhwani <e23cseu0071@bennett.edu.in>
2026-06-14 17:30:41 +0530

    Delete key.js

--- key.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const key = process.env.GEMINI_API_KEY;
-  if (!key) {
-    return res.status(404).json({ error: 'API key not configured' });
-  }
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json({ key });
-}
